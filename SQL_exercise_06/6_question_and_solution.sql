6588a3195046ff55958ce88258df8e5c8a81410c
suchip9 <48475934+suchip9@users.noreply.github.com>
2019-03-12 15:52:25 +0530

    Update 6_question_and_solution.sql
    According to question we should List all the scientists' names and their projects' names separately,but the previous query is showing only projects name.

--- SQL_exercise_06/6_question_and_solution.sql
+++ SQL_exercise_06/6_question_and_solution.sql
@@ -5,7 +5,7 @@
 -- and the hours worked by that scientist on each project, 
 -- in alphabetical order of project name, then scientist name.
 
-SELECT   S.Name, P.Name, P.Hours
+SELECT   S.Name Scientist, P.Name Project, P.Hours
 FROM     Scientists S 
          INNER JOIN AssignedTo A ON S.SSN=A.Scientist
          INNER JOIN Projects P ON A.Project=P.Code
